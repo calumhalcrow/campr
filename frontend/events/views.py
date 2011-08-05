451220e6e6695c59cecf762f72a9aeaa08011724
Calum Halcrow <calumhalcrow@gmail.com>
2011-08-05 21:56:09 +0800

    Get user's profile image URL via tweepy.

--- frontend/events/views.py
+++ frontend/events/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from social_auth import __version__ as version
 from frontend.events.models import Event, Status
+import tweepy
 
 # Create your views here.
 @login_required
@@ -13,12 +14,15 @@ def dashboard(request, event_id):
     user = request.user
     status = Status.objects.filter(user=user.id).latest('time')
 
+    twitter_user = tweepy.api.get_user(user.username)
+
     ctx = {'accounts': request.user.social_auth.all(),
            'version': version,
            'last_login': request.session.get('social_auth_last_login_backend'),
            'user': user,
            'event': event,
            'status': status,
+            'profile_image_url': twitter_user.profile_image_url,
            }
     return render_to_response('events/dashboard.html', ctx, RequestContext(request))
 
